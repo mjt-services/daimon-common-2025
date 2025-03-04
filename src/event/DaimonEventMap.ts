5c0287c11c353eae17f0a0ebf563a5b7433eb607
Matt Taylor <matt@matthewjosephtaylor.com>
2025-03-04 11:10:26 -0600

    fix typo on DaimonEventMap

--- src/event/DaimonEventMap.ts
+++ src/event/DaimonEventMap.ts
@@ -3,6 +3,6 @@ import type { EventMap, PartialSubject } from "@mjt-engine/message";
 export type StopSubject = PartialSubject<"stop">;
 export type AlarmSubject = PartialSubject<"alarm">;
 
-export type DiamonEventMap =
+export type DaimonEventMap =
   | EventMap<StopSubject, string>
   | EventMap<AlarmSubject, string>;
