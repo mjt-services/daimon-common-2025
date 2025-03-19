b6bdd70616dd807ac823b53b337cfc5014fdf2d7
Matt Taylor <matt@matthewjosephtaylor.com>
2025-03-19 09:40:14 -0500

    Update version to 2025.3.19-0940

--- dist/type/Daimon.d.ts
+++ dist/type/Daimon.d.ts
@@ -1,8 +1,10 @@
 import type { ObjectStore } from "@mjt-services/data-common-2025";
 import type { CharaCardV2 } from "./CharaCardV2";
 import type { DaimonCharaCardV2Extensions } from "./DaimonCharaCardV2Extensions";
-export type Daimon = {
-    id: string;
+export type BaseDaimon = {
     chara: CharaCardV2<DaimonCharaCardV2Extensions>;
 };
+export type Daimon = {
+    id: string;
+} & BaseDaimon;
 export declare const DAIMON_OBJECT_STORE: ObjectStore<Daimon>;
