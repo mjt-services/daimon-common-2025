eba943b3a7574695987c8d2219aa3ce4d5d28b9f
Matt Taylor <matt@matthewjosephtaylor.com>
2025-03-28 12:40:32 -0500

    Update version to 2025.3.28-1240

--- dist/daimon/findDaimonsByRoom.d.ts
+++ dist/daimon/findDaimonsByRoom.d.ts
@@ -1,4 +1,5 @@
 import type { MessageConnectionInstance } from "@mjt-engine/message/dist/createConnection";
 import { type DataConnectionMap } from "@mjt-services/data-common-2025";
 import { type Daimon } from "../type/Daimon";
-export declare const findDaimonsByRoom: <M extends DataConnectionMap>(con: MessageConnectionInstance<M>) => (roomId: string) => Promise<Daimon[]>;
+import type { Room } from "../type/Room";
+export declare const findDaimonsByRoom: <M extends DataConnectionMap>(con: MessageConnectionInstance<M>) => (roomIdOrRoom: string | Room) => Promise<Daimon[]>;
--- dist/daimon/findDaimonsByRoom.js
+++ dist/daimon/findDaimonsByRoom.js
@@ -1,6 +1,13 @@
 import { Datas, LINK_OBJECT_STORE, } from "@mjt-services/data-common-2025";
 import { DAIMON_OBJECT_STORE } from "../type/Daimon";
-export const findDaimonsByRoom = (con) => async (roomId) => {
+import { isDefined } from "@mjt-engine/object";
+export const findDaimonsByRoom = (con) => async (roomIdOrRoom) => {
+    const roomId = typeof roomIdOrRoom === "string" ? roomIdOrRoom : roomIdOrRoom.id;
+    const room = typeof roomIdOrRoom === "string"
+        ? (await Datas.get(con)({
+            key: roomId,
+        }))
+        : roomIdOrRoom;
     const ids = (await Datas.search(con)({
         from: LINK_OBJECT_STORE,
         query: `values(@)[?roomId == '${roomId}'].daimonId | []`,
@@ -10,6 +17,15 @@ export const findDaimonsByRoom = (con) => async (roomId) => {
         from: DAIMON_OBJECT_STORE,
         query,
     }));
+    // get all daimons in the parent room
+    if (isDefined(room.parentId)) {
+        const parentDaimons = await findDaimonsByRoom(con)(room.parentId);
+        for (const parentDaimon of parentDaimons) {
+            if (!daimons.some((d) => d.id === parentDaimon.id)) {
+                daimons.push(parentDaimon);
+            }
+        }
+    }
     return daimons;
 };
 //# sourceMappingURL=findDaimonsByRoom.js.map
