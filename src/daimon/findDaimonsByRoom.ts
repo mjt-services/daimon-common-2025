2af547088bc24bfc921a38ce35c55614ff84e3fd
Matt Taylor <matt@matthewjosephtaylor.com>
2025-03-28 12:40:21 -0500

    search up room hierarchy for daimons by room

--- src/daimon/findDaimonsByRoom.ts
+++ src/daimon/findDaimonsByRoom.ts
@@ -5,10 +5,21 @@ import {
   type DataConnectionMap,
 } from "@mjt-services/data-common-2025";
 import { DAIMON_OBJECT_STORE, type Daimon } from "../type/Daimon";
+import { isDefined } from "@mjt-engine/object";
+import type { Room } from "../type/Room";
 
 export const findDaimonsByRoom =
   <M extends DataConnectionMap>(con: MessageConnectionInstance<M>) =>
-  async (roomId: string): Promise<Daimon[]> => {
+  async (roomIdOrRoom: string | Room): Promise<Daimon[]> => {
+    const roomId =
+      typeof roomIdOrRoom === "string" ? roomIdOrRoom : roomIdOrRoom.id;
+    const room =
+      typeof roomIdOrRoom === "string"
+        ? ((await Datas.get(con)({
+            key: roomId,
+          })) as Room)
+        : roomIdOrRoom;
+
     const ids = (await Datas.search(con)({
       from: LINK_OBJECT_STORE,
       query: `values(@)[?roomId == '${roomId}'].daimonId | []`,
@@ -20,7 +31,15 @@ export const findDaimonsByRoom =
       from: DAIMON_OBJECT_STORE,
       query,
     })) as Daimon[];
+
+    // get all daimons in the parent room
+    if (isDefined(room.parentId)) {
+      const parentDaimons = await findDaimonsByRoom(con)(room.parentId);
+      for (const parentDaimon of parentDaimons) {
+        if (!daimons.some((d) => d.id === parentDaimon.id)) {
+          daimons.push(parentDaimon);
+        }
+      }
+    }
     return daimons;
   };
-
-
